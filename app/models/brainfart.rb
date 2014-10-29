63aebd94ea4ec20b338622b4f3d36b7a2a845d80
somini <somini29@yandex.com>
2014-10-29 02:34:09 +0000

    Exercises
    Extra validation. WARNING: crappy regex

--- app/models/brainfart.rb
+++ app/models/brainfart.rb
@@ -1,5 +1,6 @@
 class Brainfart < ActiveRecord::Base
   belongs_to :user
   validates :content,
-    length: { minimum: 10, maximum:404 }
+    length: { minimum: 10, maximum:404 },
+    presence: true # Exists, and it's not empty
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+
   has_many :brainfarts
+
+  validates :nick,
+    presence: true
+  validates :mail,
+    presence: true,
+    format: /[a-zA-Z0-9]+@[a-zA-Z0-9]+[.][a-zA-Z0-9]+/
+
 end
